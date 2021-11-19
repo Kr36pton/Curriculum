6469aaf799f25f95e179741fef7bc47c3732f721
ManuelMesa <jmmesa37@misena.edu.co>
2021-11-18 19:43:16 -0500

    slider portafolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,33 @@ const navbarnav = document.getElementsByClassName('navbar_nav')[0]
 togglebutton.addEventListener('click', () => {
     navbarnav.classList.toggle('active')
 })
+
+let slider = document.querySelector(".slider_container")
+let sliderIndividual = document.querySelectorAll(".slider_content")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 4000;
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform 1s";
+    contador++;
+
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        },1500)
+    }
+}
